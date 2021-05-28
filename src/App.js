2fa03921a3138d3d23e2ba9bc4393fa44f6f96a0
HYivisir <843702140@qq.com>
2021-05-28 17:10:52 +0800

    构建基本导航栏，折线图

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { HashRouter as Router, Link, Route } from "react-router-dom";
-import { Menu } from "antd";
-import Mail from "./views/Mail/Mail";
+import { Menu, Layout } from "antd";
+import Statistic from "./views/Statistic/Statistic";
 import Map from "./views/Map/Map";
 import {
-  MailOutlined,
-  SettingOutlined,
-  SearchOutlined,
+  LineChartOutlined,
+  BoxPlotOutlined
 } from "@ant-design/icons";
 
 // less样式
 import "./App.less";
+import { Footer } from "antd/lib/layout/layout";
 
-const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 class App extends React.Component {
   state = {
-    current: "mail",
+    current: "statistic",
   };
 
   handleClick = (e) => {
@@ -27,35 +27,33 @@ class App extends React.Component {
   render() {
     const { current } = this.state;
     return (
-      <div className="view">
-        <Router className="view">
-          <Menu
-            onClick={this.handleClick}
-            selectedKeys={[current]}
-            mode="horizontal"
-          >
-            <Menu.Item key="mail" icon={<MailOutlined />}>
-              <Link to="mail">邮箱</Link>
-            </Menu.Item>
-            <Menu.Item key="map" icon={<SearchOutlined />}>
-              <Link to="map">地图</Link>
-            </Menu.Item>
-            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="二级菜单">
-              <Menu.ItemGroup title="Item 1">
-                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                <Menu.Item key="setting:2">Option 2</Menu.Item>
-              </Menu.ItemGroup>
-              <Menu.ItemGroup title="Item 2">
-                <Menu.Item key="setting:3">Option 3</Menu.Item>
-                <Menu.Item key="setting:4">Option 4</Menu.Item>
-              </Menu.ItemGroup>
-            </SubMenu>
-          </Menu>
+      <Router>
+        <Layout className="layout">
+          <Header className="icdm-header flex-start">
+            <div className="icdm-logo flex-center">ICDM</div>
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[current]}
+              mode="horizontal"
+              theme="dark"
+            >
+              <Menu.Item key="statistic" icon={<LineChartOutlined />}>
+                <Link to="Statistic">Statistic</Link>
+              </Menu.Item>
+              <Menu.Item key="map" icon={<BoxPlotOutlined />}>
+                <Link to="map">Map</Link>
+              </Menu.Item>
+            </Menu>
+          </Header>
 
-          <Route path="/mail" component={Mail}></Route>
-          <Route path="/map" component={Map}></Route>
-        </Router>
-      </div>
+          <Content className="view">
+            <Route path="/statistic" component={Statistic}></Route>
+            <Route path="/map" component={Map}></Route>
+          </Content>
+
+          <Footer className="icdm-footer flex-center">ICDM Created By QGStudio</Footer>
+        </Layout>
+      </Router>
     );
   }
 }
--- src/views/Map/Map.js
+++ src/views/Map/Map.js
@@ -1,39 +1,63 @@
-import React, { Component } from 'react';
-import './Map.less'
+import React, { Component } from "react";
+import "./Map.less";
+import { Divider } from "antd";
 const AMap = window.AMap;
 
 class Map extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rmap : null
-        }
-    }
-    componentDidMount() {
-        this.rmap = new AMap.Map('container', {
-            zoom : 9,
-            center : [113,23]
-        });
-        // 使用插件
-        let that = this;
-        AMap.plugin(['AMap.ToolBar'],function() {
-            let toolbar = new AMap.ToolBar({
-                position : 'LT'
-            });
-            that.rmap.addControl(toolbar);
-        })
-    }
-    componentDidUpdate(prevProps, prevState) {
-        // this.rmap && this.rmap.destory();
-        console.log(this.rmap);
-    }
-    render() { 
-        return ( 
-            <div className="mapCmp">
-                <div id="container" style={{ width:"100%", height:"100%" }}></div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      map0: null,
+      map1: null,
+      map2: null
+    };
+  }
+  componentDidMount() {
+    this.map0 = new AMap.Map("originMap", {
+      zoom: 11,
+      center: [113.280637, 23.125178],
+      mapStyle: 'amap://styles/a63016a6478fb6d0589f71aa183b8e69'
+    });
+    this.map0 = new AMap.Map("dpstarMap", {
+      zoom: 11,
+      center: [113.280637, 23.125178],
+      mapStyle: 'amap://styles/a63016a6478fb6d0589f71aa183b8e69'
+
+    });
+    this.map0 = new AMap.Map("qgMap", {
+      zoom: 11,
+      center: [113.280637, 23.125178],
+      mapStyle: 'amap://styles/a63016a6478fb6d0589f71aa183b8e69'
+    });
+    // 使用插件
+    let that = this;
+    AMap.plugin(["AMap.ToolBar","AMap.DistrictSearch"], function () {
+      let districtSearch = new AMap.DistrictSearch({
+        level : 'city',
+        subdistrict: 0
+      })
+
+      districtSearch.search('广州',function(status,res) {
+        console.log(res);
+      })
+    });
+  }
+  render() {
+    return (
+      <div className="mapCmp flex-center">
+        <div className="map-container flex-start-col origin-map">
+          <Divider orientation="left">Origin</Divider>
+          <div id="originMap" style={{ width: "500px", height: "400px" }}></div>
+        </div>
+        <div className="map-container flex-start-col analyzed-map">
+          <Divider orientation="left">DP-star</Divider>
+          <div id="dpstarMap" style={{ width: "500px", height: "400px" }}></div>
+          <Divider orientation="left">DP-QG</Divider>
+          <div id="qgMap" style={{ width: "500px", height: "400px" }}></div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Map;
